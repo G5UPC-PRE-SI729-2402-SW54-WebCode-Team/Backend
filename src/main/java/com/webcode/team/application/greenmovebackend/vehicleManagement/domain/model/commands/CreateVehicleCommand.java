c969f68af5a003853da4f1de0fae71bcc97fa90e
Al3xiel <gabriel280104casas@gmail.com>
2024-11-12 00:49:56 -0500

    docs(vehicles): added docs to commands and queries.

--- src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/commands/CreateVehicleCommand.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/commands/CreateVehicleCommand.java
@@ -1,5 +1,16 @@
 package com.webcode.team.application.greenmovebackend.vehicleManagement.domain.model.commands;
 
+/**
+ * CreateVehicleCommand
+ * @summary
+ * This record represents the command to create a vehicle.
+ * @param name the name of the vehicle
+ * @param urlImage the url of the image of the vehicle
+ * @param status the status of the vehicle
+ * @param type the type of the vehicle
+ * @param ownerId the id of the owner of the vehicle
+ * @since 1.0
+ */
 public record CreateVehicleCommand(
         String name,
         String urlImage,
