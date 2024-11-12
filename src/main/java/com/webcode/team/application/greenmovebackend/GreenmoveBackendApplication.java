f910314264a3f5d2e6838675eef0111ad1f57092
Al3xiel <gabriel280104casas@gmail.com>
2024-11-11 20:26:10 -0500

    chore: setup project.

---
+++ src/main/java/com/webcode/team/application/greenmovebackend/GreenmoveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webcode.team.application.greenmovebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenmoveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenmoveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webcode/team/application/greenmovebackend/GreenmoveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcode.team.application.greenmovebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenmoveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
