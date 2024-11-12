8bb533b4f39b548adcc8b5561ba1d4a167a47ca4
Al3xiel <gabriel280104casas@gmail.com>
2024-11-12 14:11:20 -0500

    docs(memberships): added queries and commands services.

--- src/main/java/com/webcode/team/application/greenmovebackend/membershipManagement/domain/services/MembershipCommandService.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/membershipManagement/domain/services/MembershipCommandService.java
@@ -1,4 +1,14 @@
 package com.webcode.team.application.greenmovebackend.membershipManagement.domain.services;
 
+import com.webcode.team.application.greenmovebackend.membershipManagement.domain.model.aggregates.Membership;
+import com.webcode.team.application.greenmovebackend.membershipManagement.domain.model.commands.CreateMembershipCommand;
+import com.webcode.team.application.greenmovebackend.membershipManagement.domain.model.commands.DeleteMembershipCommand;
+import com.webcode.team.application.greenmovebackend.membershipManagement.domain.model.commands.UpdateMembershipCommand;
+
+import java.util.Optional;
+
 public interface MembershipCommandService {
+    Optional<Membership> handle(CreateMembershipCommand command);
+    void handle(DeleteMembershipCommand command);
+    Optional<Membership> handle(UpdateMembershipCommand command);
 }
--- src/main/java/com/webcode/team/application/greenmovebackend/membershipManagement/domain/services/MembershipQueryService.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/membershipManagement/domain/services/MembershipQueryService.java
@@ -1,4 +1,10 @@
 package com.webcode.team.application.greenmovebackend.membershipManagement.domain.services;
 
+import com.webcode.team.application.greenmovebackend.membershipManagement.domain.model.aggregates.Membership;
+import com.webcode.team.application.greenmovebackend.membershipManagement.domain.model.queries.GetMembershipByTenantIdQuery;
+
+import java.util.Optional;
+
 public interface MembershipQueryService {
+    Optional<Membership> handle(GetMembershipByTenantIdQuery query);
 }
