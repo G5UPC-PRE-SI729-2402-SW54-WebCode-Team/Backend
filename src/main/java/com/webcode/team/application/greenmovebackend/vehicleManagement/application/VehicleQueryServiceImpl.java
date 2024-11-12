19e54b47fa7505393dfedf6ba5862ae33e1c3da6
Al3xiel <gabriel280104casas@gmail.com>
2024-11-12 10:57:01 -0500

    feat(vehicles): added query implementation.

--- src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/application/VehicleQueryServiceImpl.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/application/VehicleQueryServiceImpl.java
@@ -4,6 +4,8 @@ import com.webcode.team.application.greenmovebackend.vehicleManagement.domain.mo
 import com.webcode.team.application.greenmovebackend.vehicleManagement.domain.model.queries.GetAllVehiclesByOwnerIdQuery;
 import com.webcode.team.application.greenmovebackend.vehicleManagement.domain.model.queries.GetVehicleByIdQuery;
 import com.webcode.team.application.greenmovebackend.vehicleManagement.domain.services.VehicleQueryService;
+import com.webcode.team.application.greenmovebackend.vehicleManagement.infrastructure.persistence.jpa.repositories.OwnerRepository;
+import com.webcode.team.application.greenmovebackend.vehicleManagement.infrastructure.persistence.jpa.repositories.VehicleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,13 +13,17 @@ import java.util.Optional;
 
 @Service
 public class VehicleQueryServiceImpl implements VehicleQueryService {
+
+    private final VehicleRepository vehicleRepository;
+    public VehicleQueryServiceImpl(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
     @Override
     public List<Vehicle> handle(GetAllVehiclesByOwnerIdQuery query) {
-        return List.of();
+        return this.vehicleRepository.findByOwnerId(query.ownerId());
     }
-
     @Override
     public Optional<Vehicle> handle(GetVehicleByIdQuery query) {
-        return Optional.empty();
+        return this.vehicleRepository.findById(query.vehicleId());
     }
 }
