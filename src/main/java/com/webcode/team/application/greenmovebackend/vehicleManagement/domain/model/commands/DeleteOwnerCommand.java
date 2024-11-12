058ecef8b768e1e621e7cc3cee1ed7f0d1d7235e
Al3xiel <gabriel280104casas@gmail.com>
2024-11-12 00:49:44 -0500

    docs(owners): added docs to commands and queries.

--- src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/commands/DeleteOwnerCommand.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/commands/DeleteOwnerCommand.java
@@ -1,4 +1,10 @@
 package com.webcode.team.application.greenmovebackend.vehicleManagement.domain.model.commands;
 
+/**
+ * DeleteOwnerCommand
+ * @summary this record represents the command to delete an owner
+ * @param ownerId the owner id
+ * @since 1.0
+ */
 public record DeleteOwnerCommand(Long ownerId) {
 }
