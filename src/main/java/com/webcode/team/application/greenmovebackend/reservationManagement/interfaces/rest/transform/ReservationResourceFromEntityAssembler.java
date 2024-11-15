491b4521fe0076e23debd3d4b5068b2ab1f5804c
Al3xiel <gabriel280104casas@gmail.com>
2024-11-15 12:59:54 -0500

    fix(reservations): added assemblers.

---
+++ src/main/java/com/webcode/team/application/greenmovebackend/reservationManagement/interfaces/rest/transform/ReservationResourceFromEntityAssembler.java
@@ -0,0 +1,22 @@
+package com.webcode.team.application.greenmovebackend.reservationManagement.interfaces.rest.transform;
+
+import com.webcode.team.application.greenmovebackend.membershipManagement.interfaces.rest.transform.TenantResourceFromEntityAssembler;
+import com.webcode.team.application.greenmovebackend.reservationManagement.domain.model.aggregates.Reservation;
+import com.webcode.team.application.greenmovebackend.reservationManagement.interfaces.rest.resources.ReservationResource;
+import com.webcode.team.application.greenmovebackend.vehicleManagement.interfaces.rest.transform.OwnerResourceFromEntityAssembler;
+
+public class ReservationResourceFromEntityAssembler {
+    public static ReservationResource toResourceFromEntity(Reservation reservation) {
+        return new ReservationResource(
+                reservation.getId(),
+                reservation.getReservationCode(),
+                reservation.getStartDate().toString(),
+                reservation.getEndDate().toString(),
+                reservation.getLatitude(),
+                reservation.getLongitude(),
+                OwnerResourceFromEntityAssembler.toResourceFromEntity(reservation.getOwner()),
+                TenantResourceFromEntityAssembler.toResourceFromEntity(reservation.getTenant()),
+                ReservationVehicleFromEntityAssembler.toResourceFromEntity(reservation.getVehicle())
+        );
+    }
+}
