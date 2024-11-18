7f6790292760e5c68ddb02284db449d0b5a2b29a
Al3xiel <gabriel280104casas@gmail.com>
2024-11-18 17:29:36 -0500

    fix: deleted unused validation.

--- src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/valueobjects/StreetAddress.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/valueobjects/StreetAddress.java
@@ -30,20 +30,4 @@ public record StreetAddress(
     public String getStreetAddress(){
         return String.format("%s %s %s %s %s", street, number, city, postalCode, country);
     }
-
-    public StreetAddress {
-        if (street == null || street.isBlank()) {
-            throw new IllegalArgumentException("Street is required");
-        }
-        if (city == null || city.isBlank()) {
-            throw new IllegalArgumentException("City is required");
-        }
-
-        if (postalCode == null || postalCode.isBlank()) {
-            throw new IllegalArgumentException("Postal code is required");
-        }
-        if (country == null || country.isBlank()) {
-            throw new IllegalArgumentException("Country is required");
-        }
-    }
 }
