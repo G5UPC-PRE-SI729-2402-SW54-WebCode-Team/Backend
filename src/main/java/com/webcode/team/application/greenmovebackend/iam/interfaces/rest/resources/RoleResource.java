37de115828bf5d6d09f33fcc30ef4347bd4f939c
Al3xiel <gabriel280104casas@gmail.com>
2024-11-18 08:04:57 -0500

    feat(iam): added resources.

---
+++ src/main/java/com/webcode/team/application/greenmovebackend/iam/interfaces/rest/resources/RoleResource.java
@@ -0,0 +1,4 @@
+package com.webcode.team.application.greenmovebackend.iam.interfaces.rest.resources;
+
+public record RoleResource(Long id, String name) {
+}
---
+++ src/main/java/com/webcode/team/application/greenmovebackend/iam/interfaces/rest/resources/SignInResource.java
@@ -0,0 +1,4 @@
+package com.webcode.team.application.greenmovebackend.iam.interfaces.rest.resources;
+
+public record SignInResource(String username, String password) {
+}
