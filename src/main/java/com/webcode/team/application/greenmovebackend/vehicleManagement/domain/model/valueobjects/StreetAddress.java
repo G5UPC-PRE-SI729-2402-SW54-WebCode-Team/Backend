bdd3bd09216b389845feb25eb24570e28fc6de74
enriquevillon25 <enriquevillon2597@gmail.com>
2024-11-18 20:24:08 -0500

    feat/database-config-add-new-pipeline

--- src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/valueobjects/StreetAddress.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/valueobjects/StreetAddress.java
@@ -32,18 +32,18 @@ public record StreetAddress(
     }
 
     public StreetAddress {
-        if (street == null || street.isBlank()) {
-            throw new IllegalArgumentException("Street is required");
-        }
-        if (city == null || city.isBlank()) {
-            throw new IllegalArgumentException("City is required");
-        }
-
-        if (postalCode == null || postalCode.isBlank()) {
-            throw new IllegalArgumentException("Postal code is required");
-        }
-        if (country == null || country.isBlank()) {
-            throw new IllegalArgumentException("Country is required");
-        }
+//        if (street == null || street.isBlank()) {
+//            throw new IllegalArgumentException("Street is required");
+//        }
+//        if (city == null || city.isBlank()) {
+//            throw new IllegalArgumentException("City is required");
+//        }
+//
+//        if (postalCode == null || postalCode.isBlank()) {
+//            throw new IllegalArgumentException("Postal code is required");
+//        }
+//        if (country == null || country.isBlank()) {
+//            throw new IllegalArgumentException("Country is required");
+//        }
     }
 }
