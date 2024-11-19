7f2786a5b8988dc9be6343d8cbe3d0961c10e6e4
enriquevillon25 <enriquevillon2597@gmail.com>
2024-11-18 20:27:04 -0500

    feat/config-dependencys

--- src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/valueobjects/StreetAddress.java
+++ src/main/java/com/webcode/team/application/greenmovebackend/vehicleManagement/domain/model/valueobjects/StreetAddress.java
@@ -31,19 +31,4 @@ public record StreetAddress(
         return String.format("%s %s %s %s %s", street, number, city, postalCode, country);
     }
 
-    public StreetAddress {
-//        if (street == null || street.isBlank()) {
-//            throw new IllegalArgumentException("Street is required");
-//        }
-//        if (city == null || city.isBlank()) {
-//            throw new IllegalArgumentException("City is required");
-//        }
-//
-//        if (postalCode == null || postalCode.isBlank()) {
-//            throw new IllegalArgumentException("Postal code is required");
-//        }
-//        if (country == null || country.isBlank()) {
-//            throw new IllegalArgumentException("Country is required");
-//        }
-    }
 }
